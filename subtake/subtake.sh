3a774dbc56bc0675d437faa356e8c88f118ea3d0
gilfoyle97 <edoardott@gmail.com>
2021-05-15 22:37:02 +0200

    Update subtake.sh

--- subtake/subtake.sh
+++ subtake/subtake.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-RED='\033[0;31m'
+RED='\033[0;31m' # Red
 NC='\033[0m' # No Color
 
+# Read line on stdin
 while read line
 do
     output=$( dig "$line" | grep CNAME )
+    # if output is not empty
     if [ ! -z "$output" ]
+    # print the CNAME record from dig
     then echo -e "${RED}$line${NC}"; dig "$line" | grep CNAME; echo ""
     echo "---------------------------"
     fi
