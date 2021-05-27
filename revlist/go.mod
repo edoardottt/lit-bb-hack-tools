87c808fac904b0ae2b17050ffcd0e8172e41e8f1
edoardottt <edoardott@gmail.com>
2021-05-27 19:53:45 +0200

    update

---
+++ revlist/go.mod
@@ -0,0 +1,3 @@
+module github.com/edoardottt/lit-bb-hack-tools/revlist
+
+go 1.16
---
+++ revlist/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	input := ScanTargets()
+	output := Reverse(removeDuplicateValues(input))
+	for _, elem := range output {
+		fmt.Println(elem)
+	}
+}
+
+//ScanInput return the array of elements
+//taken as input on stdin.
+func ScanTargets() []string {
+
+	var result []string
+
+	// accept domains on stdin
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		domain := strings.ToLower(sc.Text())
+		result = append(result, domain)
+	}
+	return result
+}
+
+//removeDuplicateValues
+func removeDuplicateValues(strSlice []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+
+	// If the key(values of the slice) is not equal
+	// to the already present value in new slice (list)
+	// then we append it. else we jump on another element.
+	for _, entry := range strSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
+//Reverse
+func Reverse(s []string) []string {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
+}
