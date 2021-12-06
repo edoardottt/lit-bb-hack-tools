a8a855644ee9ce1196b01af583638510485975b0
edoardottt <edoardott@gmail.com>
2021-12-06 19:08:05 +0100

    update

--- Makefile
+++ Makefile
@@ -23,6 +23,7 @@ build:
 	@cd bbscope && go build -o bbscope && sudo mv bbscope /usr/bin && echo "[ + ] bbscope installed!"
 	@cd eapath && go build -o eapath && sudo mv eapath /usr/bin && echo "[ + ] eapath installed!"
 	@cd rpfu && go build -o rpfu && sudo mv rpfu /usr/bin && echo "[ + ] rpfu installed!"
+	@cd rapwp && go build -o rapwp && sudo mv rapwp /usr/bin && echo "[ + ] rapwp installed!"
 	@echo Done!
 
 clean:
@@ -47,6 +48,7 @@ clean:
 	@sudo rm -rf /usr/bin/bbscope
 	@sudo rm -rf /usr/bin/eapath
 	@sudo rm -rf /usr/bin/rpfu
+	@sudo rm -rf /usr/bin/rapwp
 	@echo Cleaned everything!
 
 update:
---
+++ rapwp/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+)
+
+func main() {
+	helpPtr := flag.Bool("h", false, "Show usage.")
+	payloadPtr := flag.String("p", "", "Input payload.")
+	flag.Parse()
+	if *helpPtr {
+		help()
+	}
+	if *payloadPtr != "" {
+		input := ScanTargets()
+		var result []string
+		for _, elem := range input {
+			resultString := ReplaceParameters(elem, *payloadPtr)
+			if resultString != "" {
+				result = append(result, resultString)
+			}
+
+		}
+		for _, elem := range RemoveDuplicateValues(result) {
+			fmt.Println(elem)
+		}
+	} else {
+		fmt.Println("Payload required.")
+		os.Exit(0)
+	}
+}
+
+//help shows the usage
+func help() {
+	var usage = `Take as input on stdin a list of urls and a payload and print on stdout all the unique urls with ready to use payloads.
+	$> cat urls | rapwp -p "<svg onload=alert(1)>"`
+	fmt.Println(usage)
+	os.Exit(0)
+}
+
+//ScanTargets return the array of elements
+//taken as input on stdin.
+func ScanTargets() []string {
+
+	var result []string
+	// accept domains on stdin
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		domain := strings.ToLower(sc.Text())
+		result = append(result, domain)
+	}
+	return RemoveDuplicateValues(result)
+}
+
+//RemoveDuplicateValues >
+func RemoveDuplicateValues(strSlice []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, entry := range strSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
+//ReplaceParameters >
+func ReplaceParameters(input string, payload string) string {
+	u, err := url.Parse(input)
+	if err != nil {
+		return ""
+	}
+	decodedValue, err := url.QueryUnescape(u.RawQuery)
+	if err != nil {
+		return ""
+	}
+	var queryResult = ""
+	couples := strings.Split(decodedValue, "&")
+	for _, pair := range couples {
+		values := strings.Split(pair, "=")
+		queryResult += values[0] + url.QueryEscape(payload)
+	}
+	return u.Scheme + "://" + u.Host + u.Path + "?" + queryResult
+}
