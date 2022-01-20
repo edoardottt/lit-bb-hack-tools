b15b278022555811d794834f46081c81d6e446c8
edoardottt <edoardott@gmail.com>
2022-01-20 11:46:09 +0100

    update

--- eaparam/main.go
+++ eaparam/main.go
@@ -79,7 +79,8 @@ func ExtractParameters(input string) []string {
 			!strings.Contains(values[0], " ") && !strings.Contains(values[0], "?") &&
 			!strings.Contains(values[0], "/") && !strings.Contains(values[0], "@") &&
 			!strings.Contains(values[0], "(") && !strings.Contains(values[0], ")") &&
-			!strings.Contains(values[0], "\"") && !strings.Contains(values[0], ".") {
+			!strings.Contains(values[0], "\"") && !strings.Contains(values[0], ".") &&
+			!strings.Contains(values[0], "*") {
 			result = append(result, values[0])
 		}
 	}
